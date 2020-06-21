f9303a5c229fefa286f1231f641f8063405d1646
yahuishi <38556379+yahuishi@users.noreply.github.com>
2020-06-21 18:55:02 +0800

    Update App.java

--- src/main/java/zhang/testgit/App.java
+++ src/main/java/zhang/testgit/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!111" );
         System.out.println("hah111dfdfadsfsdfdfa");
         System.out.println("hahaadfadf");
     }
